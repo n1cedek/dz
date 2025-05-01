6cf2fbc4e3311d79b1729fcfed90b6e3450dc197
n1cedek <denis.kuriga@gmail.com>
2025-05-01 16:54:35 +0300

    first

---
+++ auth/cmd/auth_client/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	desc "dz/auth/pkg/w1"
+	"github.com/brianvoe/gofakeit"
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+	"log"
+	"time"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to serve: %v", err)
+	}
+	defer conn.Close()
+
+	c := desc.NewUserAPIClient(conn)
+
+	createdID := makeCreate(ctx, c)
+
+	makeGet(ctx, c, createdID)
+	makeUpdate(ctx, c, createdID)
+	makeGet(ctx, c, createdID)
+	makeDelete(ctx, c, createdID)
+}
+
+func makeCreate(ctx context.Context, c desc.UserAPIClient) int64 {
+	req := &desc.CreateRequest{
+		Name:            gofakeit.BeerName(),
+		Email:           gofakeit.Email(),
+		Password:        "123",
+		PasswordConfirm: "123",
+		Role:            desc.Role_user,
+	}
+
+	resp, err := c.Create(ctx, req)
+	if err != nil {
+		log.Fatalf("failed to create user: %v\n", err)
+	}
+	log.Println(color.YellowString("Created user with ID: %d", resp.Id))
+	return resp.Id
+}
+
+func makeUpdate(ctx context.Context, c desc.UserAPIClient, id int64) {
+	req := &desc.UpdateRequest{
+		Id:    id,
+		Name:  wrapperspb.String(gofakeit.Name()),
+		Email: wrapperspb.String(gofakeit.Email()),
+	}
+
+	_, err := c.Update(ctx, req)
+	if err != nil {
+		log.Fatalf("failed to update: %v", err)
+	}
+	log.Println(color.YellowString("Updated user with ID: %d", id))
+}
+
+func makeDelete(ctx context.Context, c desc.UserAPIClient, id int64) {
+	req := &desc.DeleteRequest{Id: id}
+
+	_, err := c.Delete(ctx, req)
+	if err != nil {
+		log.Fatalf("failed to delete user: %v", err)
+	}
+
+	log.Println(color.YellowString("Deleted user with ID: %d", id))
+}
+
+func makeGet(ctx context.Context, c desc.UserAPIClient, id int64) {
+	req := &desc.GetRequest{Id: id}
+
+	resp, err := c.Get(ctx, req)
+	if err != nil {
+		log.Fatalf("failed to get user: %v", err)
+	}
+	log.Printf(color.RedString("User info:\n", color.GreenString("%+v", resp)))
+}
---
+++ auth/cmd/auth_service/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	desc "dz/auth/pkg/w1"
+	"fmt"
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
+)
+
+const (
+	idCreate = 1
+	grpcPort = 50051
+)
+
+type UServer struct {
+	desc.UnimplementedUserAPIServer
+}
+
+func (s *UServer) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("ID: %d", req.GetId())
+	return &desc.GetResponse{
+		Id:        req.GetId(),
+		Name:      gofakeit.BeerName(),
+		Email:     gofakeit.Email(),
+		Role:      desc.Role_user,
+		CreatedAt: timestamppb.New(gofakeit.Date()),
+		UpdatedAt: timestamppb.New(gofakeit.Date()),
+	}, nil
+}
+
+func (s *UServer) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Created user: %+v\n", req)
+	return &desc.CreateResponse{Id: idCreate}, nil
+}
+
+func (s *UServer) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("Update user: %+v\n", req)
+	return &emptypb.Empty{}, nil
+}
+
+func (s *UServer) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Delete user: %+v\n", req)
+	return &emptypb.Empty{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen server: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserAPIServer(s, &UServer{})
+
+	log.Printf("Server listening at: %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
+
+}
